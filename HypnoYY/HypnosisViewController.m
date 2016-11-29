ee72884d5e211da3994620083b21512833ceeaf5
张嘉夫 <joseph@zhangjiafudeMacBook-Air.local>
2016-11-29 10:05:15 +0800

    添加控件到视图

--- HypnoYY/HypnosisViewController.m
+++ HypnoYY/HypnosisViewController.m
@@ -9,6 +9,12 @@
 #import "HypnosisViewController.h"
 #import "BNRHypnosisView.h"
 
+@interface HypnosisViewController()
+
+@property (nonatomic, copy) NSArray *colors;
+
+@end
+
 @implementation HypnosisViewController
 
 - (void)loadView {
@@ -16,6 +22,15 @@
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc]init];
     //将BNRHypnosisView对象赋给视图控制器的view属性
     self.view = backgroundView;
+    UISegmentedControl *segmentedControl = [[UISegmentedControl alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    
+    for (int i = 0; i<self.colors.count; i++) {
+        [segmentedControl insertSegmentWithTitle:self.colors[i] atIndex:i animated:YES];
+    }
+    
+    [segmentedControl sizeToFit];
+    segmentedControl.center = CGPointMake([UIScreen mainScreen].bounds.size.width/2, 50);
+    [self.view addSubview:segmentedControl];
 }
 
 -(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
@@ -28,6 +43,7 @@
         UIImage *i = [UIImage imageNamed:@"Hypno.png"];
         // 将UIImage对象赋给标签项的image属性
         self.tabBarItem.image = i;
+        self.colors = @[@"红",@"绿",@"蓝"];
     }
     return self;
 }
